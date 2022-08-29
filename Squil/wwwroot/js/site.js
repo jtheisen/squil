bfc4a8922df3bed29c8a23f576e0baf6ece4c589
jtheisen <jens.theisen@github.com>
2022-08-30 01:06:07 +0200

    Have custom events working

--- Squil/wwwroot/js/site.js
+++ Squil/wwwroot/js/site.js
@@ -3,19 +3,15 @@
 
 // Write your JavaScript code.
 
-Blazor.registerCustomEventType('hiddenbsmodal', { browserEventName: "hiddenbsmodal", createEventArgs: e => { console.info("here!"); return {}; } });
-//Blazor.registerCustomEventType('hidden.bs.modal', { createEventArgs: () => ({}) });
-
-document.addEventListener("hiddenbsmodal", () => console.info("hiddenbsmodal bubbled to root"));
+Blazor.registerCustomEventType('hiddenbsmodal', { browserEventName: "hiddenbsmodal" });
 
 function translateEvent(element, oldName, newName) {
-    console.info(`init translating event from '${oldName}' to '${newName}'`);
+    //console.info(`init translating event from '${oldName}' to '${newName}'`);
     $(element).on(oldName, function () {
         //element.addEventListener(oldName, function () {
         const newEvent = new CustomEvent(newName, { bubbles: true });
-        console.info("translating event");
+        //console.info("translating event");
         element.dispatchEvent(newEvent);
-        $(element).trigger(newName);
     })
 }
 
@@ -28,15 +24,12 @@ function openBoostrapModal(element, show) {
 function getInnerText(element) {
     const result = element?.innerText ?? "";
 
-    console.info(`got text '${result}'`);
-
     return result;
 }
 
 function setInnerText(element, text) {
     if (element && element.innerText != text) {
-        console.info(`set text from '${element.innerText}' to '${text}'`);
+        //console.info(`set text from '${element.innerText}' to '${text}'`);
         element.innerText = text;
-        //window.getSelection().selectAllChildren(element);
     }
 }
