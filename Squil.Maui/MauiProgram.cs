85f0a880d4ccbd7231e4d379d6df81e81737d70a
jtheisen <jens.theisen@github.com>
2022-10-08 04:04:07 +0200

    Have the maui project also use the common assets is working, not sure yet if I like it this way

--- Squil.Maui/MauiProgram.cs
+++ Squil.Maui/MauiProgram.cs
@@ -1,28 +1,29 @@
-﻿using Microsoft.AspNetCore.Components.WebView.Maui;
-using Squil.Maui.Data;
+﻿namespace Squil.Maui;
 
-namespace Squil.Maui
+public static class MauiProgram
 {
-    public static class MauiProgram
+    public static MauiApp CreateMauiApp()
     {
-        public static MauiApp CreateMauiApp()
-        {
-            var builder = MauiApp.CreateBuilder();
-            builder
-                .UseMauiApp<App>()
-                .ConfigureFonts(fonts =>
-                {
-                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-                });
+        var builder = MauiApp.CreateBuilder();
+        builder
+            .UseMauiApp<App>()
+            .ConfigureFonts(fonts =>
+            {
+                fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+            });
 
-            builder.Services.AddMauiBlazorWebView();
+        var services = builder.Services;
+
+        services.AddMauiBlazorWebView();
 #if DEBUG
-		builder.Services.AddBlazorWebViewDeveloperTools();
+        services.AddBlazorWebViewDeveloperTools();
 #endif
 
-            builder.Services.AddSingleton<WeatherForecastService>();
+        //services.AddSingleton<ISquilConfigStore, AppSettingsSquilConfigStore>();
+        services.AddSingleton<ISquilConfigStore, LocalFileSquilConfigStore>();
+        services.AddSingleton<LiveConfiguration>();
+        services.AddSingleton<LocationQueryRunner>();
 
-            return builder.Build();
-        }
+        return builder.Build();
     }
 }
