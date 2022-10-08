2fe1e870ed36e126cc9592e972c687d2ffbfd937
jtheisen <jens.theisen@github.com>
2022-10-09 01:35:08 +0200

    Maui app looks good now too

--- Squil.Maui/MainPage.xaml.cs
+++ Squil.Maui/MainPage.xaml.cs
@@ -1,10 +1,17 @@
-﻿namespace Squil.Maui
+﻿using Microsoft.AspNetCore.Components.WebView.Maui;
+
+namespace Squil.Maui
 {
     public partial class MainPage : ContentPage
     {
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private void Button_Clicked(Object sender, EventArgs e)
+        {
+            //Blazor.Reload();
         }
     }
 }
