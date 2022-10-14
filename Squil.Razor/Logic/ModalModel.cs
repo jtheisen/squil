f5195312751a5938e919addea163f613647e15a0
jtheisen <jens.theisen@github.com>
2022-10-14 20:30:56 +0200

    Clean up dialog logic

---
+++ Squil.Razor/Logic/ModalModel.cs
@@ -0,0 +1,30 @@
+﻿namespace Squil;
+
+public class ModalModel<T>
+{
+    private readonly Action stateHasChanged;
+
+    public ModalModel(Action stateHasChanged)
+    {
+        this.stateHasChanged = stateHasChanged;
+    }
+
+    T model;
+
+    public T Model
+    {
+        get => model;
+        set
+        {
+            model = value;
+
+            Close = model != null ? () => Model = default : null;
+
+            stateHasChanged();
+        }
+    }
+
+    public void SetModel(T value) => Model = value;
+
+    public Action Close { get; private set; }
+}
