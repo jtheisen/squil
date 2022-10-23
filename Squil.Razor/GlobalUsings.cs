4c47aef4025653199c4dd9fa83b5ecc506264f23
jtheisen <jens.theisen@github.com>
2022-10-23 23:21:18 +0200

    Introduce nlog, rework the running logic in query.razor and fix a related critical null reference issue

--- Squil.Razor/GlobalUsings.cs
+++ Squil.Razor/GlobalUsings.cs
@@ -3,6 +3,7 @@ global using System.Collections.Generic;
 global using System.Diagnostics;
 global using System.Linq;
 global using System.Threading.Tasks;
+global using NLog;
 global using static TaskLedgering.LedgerControl;
 global using static RazorHelpers;
 global using StringPairs = System.Collections.Generic.IEnumerable<System.Collections.Generic.KeyValuePair<System.String, System.String>>;
