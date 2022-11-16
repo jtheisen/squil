5894733839492061142c2c4869ddcc1e8ef6f8ba
jtheisen <jens.theisen@github.com>
2022-11-16 18:25:55 +0100

    Let's try using powershell again

--- version.ps1
+++ version.ps1
@@ -1,1 +1,17 @@
-Write-Output Hello from Powershell!
+$fullversion=$env:VERSION
+
+if ($fullversion)
+{
+	$mauiWindowsManifestFile = 'Squil.Maui\Platforms\Windows\Package.appxmanifest'
+	[xml]$mauiWindowsManifestXml = Get-Content $mauiWindowsManifestFile
+	$mauiWindowsManifestXml.Package.Identity.Version = $fullversion
+	$mauiWindowsManifestXml.OuterXml | Out-File $mauiWindowsManifestFile
+}
+else
+{
+	Write-Warning 'No VERSION found in the environment'
+
+	$fullversion='dev'
+}
+
+$fullversion | Out-File version.txt
