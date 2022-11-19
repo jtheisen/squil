d73441e601f9d7f0e4418965fb2d1fbf4ecce99e
jtheisen <jens.theisen@github.com>
2022-11-19 20:53:28 +0100

    Massive reoranization of the build

--- version.ps1
+++ version.ps1
@@ -1,22 +1,69 @@
-$fullversion=$env:SQUILVERSION
-$mauiversion=$env:SQUILMAUIVERSION
-$dockertags=$env:SQUILDOCKERTAGS
+$revision = "$SQUILREVISION"
+$branch = "$BRANCHNAME$BUILD_SOURCEBRANCHNAME"
 
-if ($fullversion)
+function Format-Xml ([xml]$xml, $indent=2)
 {
-	Write-Output "New version is: $fullversion, MAUI version remains $mauiversion, docker tags are:"
-	Write-Output "$dockertags"
-
-	$mauiWindowsManifestFile = 'Squil.Maui\Platforms\Windows\Package.appxmanifest'
-	[xml]$mauiWindowsManifestXml = Get-Content $mauiWindowsManifestFile
-	$mauiWindowsManifestXml.Package.Identity.Version = $fullversion
-	$mauiWindowsManifestXml.OuterXml | Out-File $mauiWindowsManifestFile
-}
-else
-{
-	Write-Warning 'No VERSION found in the environment'
-
-	$fullversion='dev'
+    $StringWriter = New-Object System.IO.StringWriter
+    $XmlWriter = New-Object System.XMl.XmlTextWriter $StringWriter
+    $xmlWriter.Formatting = "indented"
+    $xmlWriter.Indentation = $indent
+    $xml.WriteContentTo($XmlWriter)
+    $XmlWriter.Flush()
+    $StringWriter.Flush()
+    return $StringWriter.ToString()
 }
 
-$fullversion | Out-File version.txt
+[xml]$versionXml = Get-Content version.xml
+$major = $versionXml.Version.Major
+$minor = $versionXml.Version.Minor
+$patch = $versionXml.Version.Patch
+
+$baseVersion = "$major.$minor.$patch$"
+
+$branchSuffix = if ($branch -eq "release") { "" } else { "-$branch" }
+
+$fullVersion = "$baseVersion$branchSuffix"
+
+$withMajor = "$major$branchSuffix"
+$withMinor = "$major.$minor$branchSuffix"
+$withPatch = "$major.$minor.$patch$branchSuffix"
+$withRevision = "$major.$minor.$patch.$revision$branchSuffix"
+$mauiWindows = "$major.$minor.$patch.0"
+
+$versionXml.Version.Revision = $revision
+$versionXml.Version.Display = $withRevision
+$versionXml.Version.MauiWindows = $mauiWindows
+$versionXml.Version.Tags.WithMajor = "$withMajor"
+$versionXml.Version.Tags.WithMinor = "$withMinor"
+$versionXml.Version.Tags.WithPatch = "$withPatch"
+$versionXml.Version.Tags.WithRevision = "$withRevision"
+$versionXml.Version.Tags.Latest = "latest$branchSuffix"
+
+$formattedXml = Format-Xml($versionXml)
+
+$formattedXml | Write-Output
+
+if (-not $branch)
+{
+    Write-Warning "Branch name not set, won't continue"
+
+    Break Script
+}
+
+if (-not $revision)
+{
+    Write-Warning "No revision set, won't continue'"
+
+    Break Script
+}
+
+# Update version.xml
+$formattedXml | Out-File version.xml
+
+# Update MAUI Version
+$mauiWindowsManifestFile = 'Squil.Maui\Platforms\Windows\Package.appxmanifest'
+[xml]$mauiWindowsManifestXml = Get-Content $mauiWindowsManifestFile
+$mauiWindowsManifestXml.Package.Identity.Version = $mauiWindows
+Format-Xml($mauiWindowsManifestXml) | Out-File $mauiWindowsManifestFile
+
+
