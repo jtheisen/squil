bb916d1e418897c1a1972890dcf30d7a1fd35730
jtheisen <jens.theisen@github.com>
2022-11-20 21:17:30 +0100

    Strangly a scan of a large query doesn't stall any more - investigate later

--- Squil.Core/Utils.cs
+++ Squil.Core/Utils.cs
@@ -1,3 +1,4 @@
+using NLog;
 using System.Collections.Specialized;
 using System.ComponentModel;
 using System.IO;
@@ -89,6 +90,8 @@ public static class RazorHelpers
 
 public static partial class Extensions
 {
+    static Logger log = LogManager.GetCurrentClassLogger();
+
     static readonly XmlWriterSettings xmlWriterSettings = new XmlWriterSettings { Indent = true };
 
     public static String Format(this XmlDocument document)
@@ -264,7 +267,19 @@ public static partial class Extensions
 
     public static T If<T>(this T source, Boolean predicate) => predicate ? source : default;
 
-    public static void Ignore(this Task _) { }
+    public static async void Ignore(this Task task)
+    {
+        try
+        {
+            await task;
+        }
+        catch (Exception e)
+        {
+            log.Error(e, "Ignored task terminated with exception: " + e.Message);
+
+            throw;
+        }
+    }
 
     public static void Ignore(this ValueTask _) { }
 
