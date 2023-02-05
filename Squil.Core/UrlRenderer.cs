e1036cf937e7d0fcf0626a673890aa871bd3889d
jtheisen <jens.theisen@github.com>
2023-02-05 06:21:35 +0100

    Don't use HttpUtility for percent encoding, but EscapeDataString isn't great either

--- Squil.Core/UrlRenderer.cs
+++ Squil.Core/UrlRenderer.cs
@@ -1,4 +1,6 @@
-﻿using static System.Web.HttpUtility;
+﻿using System.IO;
+using System.Text;
+using static System.Uri;
 
 namespace Squil;
 
@@ -15,9 +17,9 @@ public class UrlRenderer
 
     public String RenderUrl(IEnumerable<String> segments, IEnumerable<(String prefix, String key, String value)> queryParams)
     {
-        var path = String.Join('/', from s in segments where s != null select UrlEncode(s));
+        var path = String.Join('/', from s in segments where s != null select EscapeDataString(s));
 
-        var query = String.Join('&', from p in queryParams where p.key != null select $"{p.prefix}{UrlEncode(p.key)}={p.value}");
+        var query = String.Join('&', from p in queryParams where p.key != null select $"{p.prefix}{EscapeDataString(p.key)}={EscapeDataString(p.value)}");
 
         var lastSlashI = Math.Max(0, path.LastIndexOf('/'));
 
@@ -36,5 +38,55 @@ public class UrlRenderer
         {
             return $"{baseUrl}/{path}?{query}";
         }
+    }
+}
+
+
+public class UrlEncoder
+{
+    Boolean[] table;
+
+    public UrlEncoder()
+    {
+        var considered = "-_.~" + "!*'();:@+$,/%[]?"; //+ "=&#";
+
+        table = Enumerable.Range(0, 256).Select(i =>
+        {
+            if (i > 127) return true;
+
+            var c = (Char)i;
+
+            if (Char.IsLetterOrDigit(c)) return false;
+
+            if (considered.Contains(c)) return false;
+
+            return true;
+        }).ToArray();
+    }
+
+    public void WriteUrlEncoded(TextWriter writer, String text)
+    {
+        var utf8 = Encoding.UTF8.GetBytes(text);
+
+        foreach (var b in utf8)
+        {
+            var i = (Int32)b;
+
+            if (table[i])
+            {
+                writer.Write("%{0:x2}", i);
+            }
+            else
+            {
+                writer.Write((Char)b);
+            }
+        }
+    }
+
+    public String Encode(String text)
+    {
+        var writer = new StringWriter();
+        WriteUrlEncoded(writer, text);
+        return writer.ToString();
     }
 }
